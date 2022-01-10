5b9957ae9bc1ca24f993b8da960a0fc4dddc51e6
Hunter Hutchisson <hunter.hutchisson@gmail.com>
2022-01-09 19:23:39 -0600

    template

---
+++ src/app/store.ts
@@ -0,0 +1,29 @@
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
+// configurtion for local storage
+export const config = {
+  key: 'root',
+  storage: storage
+}
+
+const rootReducer = combineReducers({
+  counter: counterReducer
+})
+
+const persisted = persistReducer(config, rootReducer)
+
+export const store = configureStore({
+  reducer: persisted
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
