ce687f0941483dc83c8be26a614be456c65bce8d
Hunter Hutchisson <hunter.hutchisson@gmail.com>
2022-01-12 15:35:25 -0600

    bootstrap styling

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './App';
 import BaseLayout from './common/layout/BaseLayout'
